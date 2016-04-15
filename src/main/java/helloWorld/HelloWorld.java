4e0814ea04040ff35fc194ca57e603b3f7929d5a
chivchila <chivchila@gmail.com>
2016-04-15 12:33:23 +0100

    adding full directory

---
+++ src/main/java/helloWorld/HelloWorld.java
@@ -0,0 +1,21 @@
+package helloWorld;
+
+/**
+ * Created by chivchila on 15/04/2016.
+ */
+public class HelloWorld {
+
+    public int returnTwo(){
+        return 2;
+    }
+
+    public int returnThree(){
+        return 3;
+    }
+
+    public static void main(String[] args){
+
+        System.out.println("hello world 1!");
+
+    }
+}
---
+++ src/test/java/helloWorld/HelloWorldTest.java
@@ -0,0 +1,23 @@
+package helloWorld;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by chivchila on 15/04/2016.
+ */
+public class HelloWorldTest {
+    public HelloWorld hw = new HelloWorld();
+    @Test
+    public void testReturnTwo() throws Exception {
+        Assert.assertEquals(hw.returnTwo(), 2);
+    }
+
+    @Test
+    public void testReturnThree() throws Exception {
+        Assert.assertEquals(hw.returnThree(), 2);
+    }
+
+}
